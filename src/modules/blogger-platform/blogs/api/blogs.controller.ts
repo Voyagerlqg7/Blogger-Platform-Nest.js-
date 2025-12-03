db70bc7755a7e3a3426f35913dcc533c6fd20a33
Vadim Stepanenko <stepanenko.vadik@bk.ru>
2025-12-03 16:27:26 +0100

    Implementation for controllers

--- src/modules/blogger-platform/blogs/api/blogs.controller.ts
+++ src/modules/blogger-platform/blogs/api/blogs.controller.ts
@@ -14,44 +14,48 @@ import {
 import { CreateBlogDto } from '../dto/create-blog.dto';
 import { CreatePostForBlogDto } from '../dto/create-post-for-blog.dto';
 import { BlogService } from '../application/blog.service';
+import { BlogsQueryRepository } from '../infrastructure/query/blogs.query-repository';
+import { BlogsViewDto } from './view-dto/blogs.view-dto';
 
 @Controller('blogs')
 export class BlogsController {
-  constructor(private readonly blogService: BlogService) {}
+  constructor(
+    private readonly blogService: BlogService,
+    private readonly blogsQueryRepository: BlogsQueryRepository,
+  ) {}
 
   @Get()
-  async getAllBlogs(@Query() query: any) {
-    await
+  async getAllBlogs(@Query() query: any): Promise<BlogsViewDto[]> {
+    return this.blogsQueryRepository.getAll(query);
   }
 
   @Get(':id')
-  async getBlog(@Param('id') blogId: string) {
+  async getBlog(@Param('id') blogId: string): Promise<BlogsViewDto> {
+    return this.blogsQueryRepository.getByIdOrNotFoundFail(blogId);
   }
 
   @Get(':id/posts')
-  async getAllPostsFromBlog(@Param('id') blogId: string, @Query() query: any) {
-  }
+  async getAllPostsFromBlog(@Param('id') blogId: string, @Query() query: any) {}
 
   @Post(':id/posts')
   async createPostsForSpecificBlog(
     @Param('id') blogId: string,
     @Body() dto: CreatePostForBlogDto,
-  ) {
-  }
+  ) {}
 
   @Delete(':id')
   @HttpCode(HttpStatus.NO_CONTENT)
-  async deleteBlog(@Param('id') blogId: string) {
-    return;
+  async deleteBlog(@Param('id') blogId: string): Promise<void> {
+    return this.blogService.deleteBlog(blogId);
   }
 
   @Put(':id')
   async updateBlog(@Param('id') blogId: string, @Body() body: CreateBlogDto) {
-    return;
+    return this.blogService.updateBlog(blogId, body);
   }
 
   @Post()
   async createBlog(@Body() createBlogDto: CreateBlogDto) {
-    return;
+    return this.blogService.createBlog(createBlogDto);
   }
 }
--- src/modules/blogger-platform/posts/api/posts.controller.ts
+++ src/modules/blogger-platform/posts/api/posts.controller.ts
@@ -12,31 +12,49 @@ import {
 } from '@nestjs/common';
 import { CreatePostDto } from '../dto/create-post.dto';
 import { UpdatePostDto } from '../dto/update-post.dto';
+import { PostService } from '../application/posts.service';
+import { PostsQueryRepository } from '../infrastructure/query/posts.query-repository';
+import { PostsViewDto } from './view-dto/posts.view-dto';
 
 @Controller('posts')
 export class PostsController {
+  constructor(
+    private postService: PostService,
+    private postsQueryRepository: PostsQueryRepository,
+  ) {}
+
   @Get()
-  async getAllPosts(@Query() query: any) {}
+  async getAllPosts(@Query() query: any): Promise<PostsViewDto[]> {
+    return this.postsQueryRepository.getAll(query);
+  }
 
   @Get(':id')
-  async getPostById(@Param('id') postId: string) {}
+  async getPostById(@Param('id') postId: string): Promise<PostsViewDto> {
+    return this.postsQueryRepository.getByIdOrNotFoundFail(postId);
+  }
 
-  @Get(':id/comments')
+  /*@Get(':id/comments')
   async getAllCommentsFromSpecificPost(
     @Param('id') postId: string,
     @Query() query: any,
-  ) {}
+  ) {}*/
 
   @Post()
-  async createPost(@Body() newPost: CreatePostDto) {}
+  async createPost(@Body() newPost: CreatePostDto) {
+    return this.postService.createPost(newPost);
+  }
 
   @Delete(':id')
-  async deletePost(@Param('id') postId: string) {}
+  async deletePost(@Param('id') postId: string) {
+    return this.postService.deletePost(postId);
+  }
 
   @Put(':id')
   @HttpCode(HttpStatus.NO_CONTENT)
   async updatePost(
     @Param('id') postId: string,
     @Body() updatePostDto: UpdatePostDto,
-  ) {}
+  ) {
+    return this.postService.updatePost(postId, updatePostDto);
+  }
 }
