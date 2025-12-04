fe0c46c85bae0316b02c4ca0aae0aee9c5bb75a8
Vadim Stepanenko <stepanenko.vadik@bk.ru>
2025-12-04 17:02:04 +0100

    Added BloggerPlatform to app main module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserAccountsModule } from './modules/user-accounts/user-accounts.module';
+import { BloggerPlatform } from './modules/blogger-platform/blogger-platform.module';
 
 const mongoURI: string = process.env.LOCAL_MONGODB_URI!;
 
 @Module({
-  imports: [MongooseModule.forRoot(mongoURI), UserAccountsModule],
+  imports: [
+    MongooseModule.forRoot(mongoURI),
+    UserAccountsModule,
+    BloggerPlatform,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
