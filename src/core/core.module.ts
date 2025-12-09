50a61954a77fed1c1a9e14d20015884b0e17aaf0
Vadim Stepanenko <stepanenko.vadik@bk.ru>
2025-12-09 18:29:52 +0100

    moved to comment section

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -1,8 +1,10 @@
-import { Module, Global} from '@nestjs/common';
+//import { Module, Global } from '@nestjs/common';
 
+/*
 @Global()
 @Module({
     providers: [CoreConfig, LoggerService],
     exports: [CoreConfig, LoggerService],
 })
 export class CoreModule {}
+*/
