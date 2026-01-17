c6d45f2bff1a00341bf6e20dd3773d4bbcb31a41
Vadim Stepanenko <stepanenko.vadik@bk.ru>
2026-01-17 17:11:04 +0100

    ExceptionsFilters: added domain exceptions with domain codes

---
+++ src/core/exceptions/domain-exceptions-codes.ts
@@ -0,0 +1,21 @@
+export enum DomainExceptionCode {
+  NotFound = 1,
+  BadRequest = 2,
+  InternalServerError = 3,
+  Forbidden = 4,
+  ValidationError = 5,
+  LoginAlreadyExists = 6,
+
+  //auth errors
+  Unauthorized = 11,
+  EmailNotConfirmed = 12,
+  ConfirmationCodeExpired = 13,
+  PasswordRecoveryCodeExpired = 14,
+  WrongPasswordRecoveryCode = 15,
+  InvalidCredentials = 16,
+}
+
+// 1–10    — domain
+// 11–20   — auth
+// 21–30   — payments
+// 31–40   — subscriptions
