d17da5e9afc7ccaf568a10f7cdc08ef1ceda485c
Vadim Stepanenko <stepanenko.vadik@bk.ru>
2026-02-12 18:55:57 +0100

    Auth controller: installed import '@nestjs/jwt' with 'passport'

--- src/modules/user-accounts/application/JWT.service.ts
+++ src/modules/user-accounts/application/JWT.service.ts
@@ -1,23 +1,7 @@
-import jwt from 'jsonwebtoken';
-import { UserViewDto } from '../api/view-dto/users.view-dto';
-import * as process from 'node:process';
+import { JwtService } from '@nestjs/jwt';
 
-export class JWTService {
-  async createAccessToken(user: UserViewDto): Promise<string> {
-    const asscessToken: string = await jwt.sign(
-      { userId: user.id },
-      process.env.JWT_SECRET,
-      { expiresIn: '300s' },
-    );
-    return asscessToken;
-  }
+export class JWTAppService {
+    acyns createAccessToken(token: string) {
 
-  async createRefreshToken(user: UserViewDto): Promise<string> {
-    const refreshToken: string = await jwt.sign(
-      { userId: user.id },
-      process.env.JWT_SECRET,
-      { expiresIn: '20s' },
-    );
-    return refreshToken;
-  }
+    }
 }
