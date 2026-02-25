c2996a967e821ba1331bf7f18f69c4118b52dd98
Vadim Stepanenko <stepanenko.vadik@bk.ru>
2026-02-25 16:09:11 +0100

    Auth controller: added token repository

---
+++ src/modules/user-accounts/infrastructure/tokens.repository.ts
@@ -0,0 +1,25 @@
+import { InjectModel } from '@nestjs/mongoose';
+import { Injectable } from '@nestjs/common';
+import { Token } from '../domain/token.entity';
+import type { TokenModelType } from '../domain/token.entity';
+
+@Injectable()
+export class TokensRepository {
+  constructor(@InjectModel(Token.name) private tokenModel: TokenModelType) {}
+
+  async findToken(token: string): Promise<string | null> {
+    const tokenInD = await this.tokenModel.findOne({ token });
+    return tokenInD ? token : null;
+  }
+
+  async saveToken(token: string): Promise<string | null> {
+    const doc = await this.tokenModel.create({ _id: token, token });
+    console.log('SAVED TOKEN', doc.token);
+    return doc.token;
+  }
+
+  async deleteToken(token: string): Promise<true | null> {
+    const result = await this.tokenModel.deleteOne({ token });
+    return result ? true : null;
+  }
+}
