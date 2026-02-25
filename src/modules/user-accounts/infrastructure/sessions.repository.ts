02681bdeff0e28e4ef78db4dc7fb985ea5500351
Vadim Stepanenko <stepanenko.vadik@bk.ru>
2026-02-25 16:35:20 +0100

    Auth controller: added session repository

---
+++ src/modules/user-accounts/infrastructure/sessions.repository.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import type { SessionModelType } from '../domain/session.entity';
+import { Session } from '../domain/session.entity';
+
+@Injectable()
+export class SessionRepository {
+  constructor(
+    @InjectModel(Session.name) private readonly sessionModel: SessionModelType,
+  ) {}
+
+  async save(session: Session): Promise<void> {
+    await this.sessionModel.insertOne(session);
+  }
+
+  async findByUserId(userId: string) {
+    return await this.sessionModel.find({ userId }).lean();
+  }
+
+  async findByDeviceId(deviceId: string) {
+    return await this.sessionModel.findOne({ deviceId });
+  }
+
+  //TODO: next methods should be moved to DDD concepts
+  async updateLastActiveDate(
+    deviceId: string,
+    update: { lastActiveDate: Date; expirationDate?: Date },
+  ) {
+    return await this.sessionModel.updateOne({ deviceId }, { $set: update });
+  }
+
+  async deleteAllExcept(userId: string, deviceId: string) {
+    await this.sessionModel.deleteMany({ userId, deviceId: { $ne: deviceId } });
+  }
+
+  async deleteDeviceById(deviceId: string) {
+    await this.sessionModel.deleteOne({ deviceId });
+  }
+
+  async deleteExpired(now: Date) {
+    await this.sessionModel.deleteMany({ expirationDate: { $lt: now } });
+  }
+}
