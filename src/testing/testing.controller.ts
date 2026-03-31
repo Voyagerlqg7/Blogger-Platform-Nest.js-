436849d636c213172d7b01b78d1f44ca8f1e0556
Vadim Stepanenko <stepanenko.vadik@bk.ru>
2026-03-31 18:20:52 +0200

    removing tokens and session before starting server.

--- src/testing/testing.controller.ts
+++ src/testing/testing.controller.ts
@@ -8,6 +8,8 @@ import { UserDocument } from '../modules/user-accounts/domain/user.entity';
 import { CommentDocument } from '../modules/blogger-platform/comments/domain/comment.entity';
 import { PostLikeDocument } from '../modules/blogger-platform/posts/domain/post-likes.entity';
 import { CommentLikeDocument } from '../modules/blogger-platform/comments/domain/Schema/commentatorLikeInfo.schema';
+import { TokenDocument } from '../modules/user-accounts/domain/token.entity';
+import { SessionDocument } from '../modules/user-accounts/domain/session.entity';
 
 @Controller('testing')
 export class TestingController {
@@ -21,6 +23,10 @@ export class TestingController {
     private readonly postLikeModel: Model<PostLikeDocument>,
     @InjectModel('CommentLike')
     private readonly commentLikeModel: Model<CommentLikeDocument>,
+    @InjectModel('Token')
+    private readonly tokenModel: Model<TokenDocument>,
+    @InjectModel('Session')
+    private readonly sessionModel: Model<SessionDocument>,
   ) {}
 
   @Delete('all-data')
@@ -32,5 +38,7 @@ export class TestingController {
     await this.commentModel.deleteMany({});
     await this.postLikeModel.deleteMany({});
     await this.commentLikeModel.deleteMany({});
+    await this.tokenModel.deleteMany({});
+    await this.sessionModel.deleteMany({});
   }
 }
